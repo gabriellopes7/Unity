6bb18679e147ff436e7952fdb467d3e7e8ad7881
Gabriel Ladeira <gabriellopes7@yahoo.com>
2021-05-13 21:45:56 -0300

    commit2

--- 2d/intro/Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
+++ 2d/intro/Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -8,12 +8,23 @@ public class NumberWizard : MonoBehaviour
     //Start acontece quando inicializamos a engine
 
     //declarar uma variavel D aqui ela será acessivel para ambos os métodos, será chamado variável de instância pois é acessivel a toda classe ou instância
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max ;
+    int min ;
+    int guess ;
     void Start()
     {
-        
+        StartGame();
+
+       
+    }
+
+
+    //Estrutura de função
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
 
         Debug.Log("Welcome to Number wizard,yo");
         Debug.Log("Pick a number and don't tell me what it is");
@@ -21,7 +32,11 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Lowest number is: " + min);
         Debug.Log("Tell me if your number is higher or lower than 500");
         Debug.Log("Push UP = Higher, Push DOWN = Lower, Push ENTER = Correct");
+
+        max++;
+        min--;  
     }
+
 
     // Update is called once per frame
     // As variáveis do bloco de Codigo Start não são acessíveis ao bloco de Código Update, pois estão fora do escopo
@@ -29,25 +44,39 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+
+            
             min = guess;
-            guess = (max + guess) / 2;
-           
-            Debug.Log(guess);
+            NextGuess();
+
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            
             max = guess;
-            guess = (min + guess) / 2;
-            Debug.Log(guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return)) 
         {
             Debug.Log("Your number is: " + guess);
+            Debug.Log("PLAY AGAIN !");
+
+            StartGame();
         }
-        else if (guess + 1 == max || guess - min == min)
+        else if (guess + 1 == max || guess - 1 == min)
         {
             Debug.Log("Your number is: " + guess);
+            Debug.Log("PLAY AGAIN !");
+
+            StartGame();
         }
     }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is your guess higher (Press Up), lower (Press Down) or is equal to (Press Enter)  than " + guess + " ? ");
+    }
     
+
 }
